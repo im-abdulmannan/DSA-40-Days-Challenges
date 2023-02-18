85ed046c6d9243a4bf5bd53af6070c9e573e2cf1
Abdul Mannan <abdulmann1234@gmail.com>
2023-02-18 20:25:55 +0500

    Linked List

---
+++ Day-4/Linked.List.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} *first = NULL;
+
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first = new Node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (i = 1; i < n; i++)
+    {
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        cout << p->data << ", ";
+        p = p->next;
+    }
+}
+
+int main()
+{
+    int A[] = {1, 2, 3, 4, 5, 23, 45, 77};
+    create(A, 8);
+    Display(first);
+    return 0;
+}
