02ef7f5c83a9a0e70096394a304fea9319841f0a
Abdul Mannan <abdulmann1234@gmail.com>
2023-02-27 01:23:49 +0500

    Find minimum in rotated sorted array

---
+++ Day-10/Find.Minimum.In.Rotated.Sorted.Array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    int findMin(std::vector<int> &nums)
+    {
+        int minElement = nums[0];
+
+        for (int index = 0; index < nums.size(); index++)
+        {
+            if (nums[index] < minElement)
+                minElement = nums[index];
+        }
+
+        return minElement;
+    }
+};
+
+int main(void)
+{
+    Solution S;
+    std::vector<int> nums = {1, 2, 3, 4, 5, 6};
+    std::cout << S.findMin(nums);
+
+    return 0;
+}
