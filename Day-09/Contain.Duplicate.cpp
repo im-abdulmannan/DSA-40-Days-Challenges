d4bd71bf3c9cf0289d27685a504f5634aeeae254
Abdul Mannan <abdulmann1234@gmail.com>
2023-02-27 01:23:06 +0500

    Updated Contain Duplicate

--- Day-09/Contain.Duplicate.cpp
+++ Day-09/Contain.Duplicate.cpp
@@ -1,18 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
 class Solution
 {
 public:
-    bool containsDuplicate(vector<int> &nums)
+    bool containsDuplicate(std::vector<int> &nums)
     {
         if (!nums.size())
             return 0;
 
-        unordered_set<int> st;
+        std::unordered_set<int> st;
 
-        for (int i : nums)
+        for (int index : nums)
         {
-            st.insert(i);
+            st.insert(index);
         }
 
         return st.size() != nums.size();
     }
 };
+
+int main(void)
+{
+    Solution S;
+    std::vector<int> nums = {10, 5, 3, 4, 3, 5, 6};
+    if (S.containsDuplicate(nums))
+        std::cout << "Yes";
+
+    else
+        std::cout << "No";
+
+    return 0;
+}
